ef362a27795de6ebadbc01667b52bd549cb7d2c4
praniil <pparajuli15@gmail.com>
2024-10-23 08:41:16 +0545

    feat: applied gravity to the hero and handled the W keycode for jumping

--- src/state/play.rs
+++ src/state/play.rs
@@ -1,3 +1,4 @@
+use ggez::mint::Vector2;
 use ggez::{Context, GameResult};
 use ggez::event::EventHandler;
 use ggez::graphics::{self, draw, drawable_size, Color, DrawParam};
@@ -10,10 +11,36 @@ struct Arrow {
     position : (f32, f32),
     ongoing : bool,
 }
+
+struct Hero {
+    size: (f32, f32),
+    position: (f32, f32)
+}
+
+trait Gravity {
+    fn gravity(&mut self);
+}
+
+impl Gravity for Hero {
+    fn gravity(&mut self) {
+        self.position.1 += 10.0;
+        //840.0 position of hero in y
+        if self.position.1 > 840.0 {
+            self.position.1 = 840.0;
+        }
+    }
+}
+
+fn apply_gravity(items: &mut Vec<Box<dyn Gravity>>) {
+    for item in items {
+        item.gravity();
+    }
+}
+
 pub struct PlayState {
     hero_character: Image,
-    hero_character_size: (f32, f32),
-    hero_character_position: (f32, f32),
+    // hero_character_size: (f32, f32),
+    // hero_character_position: (f32, f32),
     shield_ability_position: (f32, f32),
     shiled_ability_cooldown_position: (f32, f32),
     teleport_ability_position: (f32, f32),
@@ -21,6 +48,7 @@ pub struct PlayState {
     ultimate_ability_position: (f32, f32),
     ultimate_ability_cooldown_position: (f32, f32),
     draw_arrow: bool,
+    hero: Hero,
     // arrow_ongoing: bool,
     // arrow_position: (f32, f32),
     arrows: Vec<Arrow>,
@@ -31,8 +59,12 @@ impl PlayState {
     pub fn new(ctx: &mut Context) -> GameResult<PlayState> {
         let (win_width, win_height) = drawable_size(ctx);
         let hero_character = load_image(ctx, "/home/pranil/rustProjects/demon_war/resources/copy_hero.png");
-        let hero_character_size = (95.0, 120.0);
-        let hero_character_position = (win_width/ 2.0, win_height - 2.0 * hero_character_size.1);
+        let hero = Hero {
+            size: (95.0, 120.0),
+            position: (win_width/ 2.0, win_height - 2.0 * 120.0),
+        };
+        // let hero_character_size = (95.0, 120.0);
+        // let hero_character_position = (win_width/ 2.0, win_height - 2.0 * hero_character_size.1);
         let shield_ability_position= (11.0, 11.0);
         let shiled_ability_cooldown_position = (18.0, 18.0);
         let teleport_ability_position= (31.0, 31.0);
@@ -42,7 +74,7 @@ impl PlayState {
         let draw_arrow = false;
         // let arrow_ongoing = false;
         let arrow = Arrow {
-            position : (hero_character_position.0 - 47.0 + hero_character_size.0 / 2.0, hero_character_position.1),
+            position : (hero.position.0 - 47.0 + hero.size.0 / 2.0, hero.position.1),
             ongoing : false
         };
         // let arrow_position = (hero_character_position.0 - 2.0 + hero_character_size.0 / 2.0, hero_character_position.1 - 44.0);
@@ -50,8 +82,8 @@ impl PlayState {
 
         Ok(PlayState{
             hero_character,
-            hero_character_size,
-            hero_character_position,
+            // hero_character_size,
+            // hero_character_position,
             shield_ability_position, 
             shiled_ability_cooldown_position, 
             teleport_ability_cooldown_position, 
@@ -62,12 +94,14 @@ impl PlayState {
             // arrow_position,
             // arrow_ongoing,
             arrows,
+            hero,
         })
     }
 }
 
 impl EventHandler <ggez::GameError> for PlayState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.hero.gravity();
         for arrow in &mut self.arrows {
             if arrow.ongoing == true {
                 arrow.position.1 -= 5.5;
@@ -83,13 +117,13 @@ impl EventHandler <ggez::GameError> for PlayState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, Color::from_rgb(211, 211, 211));
-        let hero_dist_rect = graphics::Rect::new(self.hero_character_position.0, self.hero_character_position.1, self.hero_character_size.0, self.hero_character_size.1);
-        let hero_draw_param  = DrawParam::default().dest([hero_dist_rect.x, hero_dist_rect.y]).scale([self.hero_character_size.0 / self.hero_character.width() as f32 , self.hero_character_size.1/ self.hero_character.height() as f32]);
+        let hero_dist_rect = graphics::Rect::new(self.hero.position.0, self.hero.position.1, self.hero.size.0, self.hero.size.1);
+        let hero_draw_param  = DrawParam::default().dest([hero_dist_rect.x, hero_dist_rect.y]).scale([self.hero.size.0 / self.hero_character.width() as f32 , self.hero.size.1/ self.hero_character.height() as f32]);
         graphics::draw(ctx, &self.hero_character, hero_draw_param)?;
 
         for arrow in &self.arrows {
             if arrow.ongoing == true {
-                let arrow = graphics::Rect::new(arrow.position.0 - 2.0 + self.hero_character_size.0 / 2.0, arrow.position.1 - 44.0, 2.0, 40.0);
+                let arrow = graphics::Rect::new(arrow.position.0 - 2.0 + self.hero.size.0 / 2.0, arrow.position.1 - 44.0, 2.0, 40.0);
                 let arrow_mess = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), arrow, Color::from_rgb(0, 0, 0)).unwrap();
                 graphics::draw(ctx, &arrow_mess, DrawParam::default()).unwrap();
             }
@@ -108,7 +142,7 @@ impl EventHandler <ggez::GameError> for PlayState {
         if button == event::MouseButton::Left {
             println!("left button in mouse clicked");
             let new_arrow = Arrow {
-                position: (self.hero_character_position.0 - 47.0 + self.hero_character_size.0 / 2.0, self.hero_character_position.1),
+                position: (self.hero.position.0 - 47.0 + self.hero.size.0 / 2.0, self.hero.position.1),
                 ongoing: true,
             };
             self.arrows.push(new_arrow);
@@ -123,12 +157,24 @@ impl EventHandler <ggez::GameError> for PlayState {
             _repeat: bool,
         ) {
         match keycode {
-            KeyCode::A => {
-                self.hero_character_position.0 -= 20.0;
-                println!("a pressed");
-            },
-            KeyCode::D => self.hero_character_position.0 += 20.0,
+            KeyCode:: A => {
+                self.hero.position.0 -= 50.0;
+            }
+            KeyCode :: W => {
+                self.hero.position.1 -= 100.0;
+                println!("pressed w");
+            }
+            KeyCode::D => {
+                self.hero.position.0 += 50.0;
+            }
             _ => {}
+            // KeyCode::A => {
+            //     self.hero.position.0 -= 20.0;
+            //     println!("a pressed");
+            // },
+            // KeyCode::D => self.hero.position.0 += 20.0,
+            // KeyCode::W => self.hero.position.1 -= 20.0,
+            // _ => {}
         }
     }
 }
